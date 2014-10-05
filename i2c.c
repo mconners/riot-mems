71037ef81c745c01bc8bf3ebca283e327106ae4e
mconners <michael_conners@hotmail.com>
2014-10-05 10:07:54 -0400

    Adding license

--- i2c.c
+++ i2c.c
@@ -1,3 +1,24 @@
+// Copyright (c) 2014 Michael Conners
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <linux/i2c.h>
@@ -48,10 +69,7 @@ int write_register(unsigned char address, unsigned char reg, unsigned char data)
     packets.nmsgs = 1;
 
     if (ioctl(fd, I2C_RDWR, &packets) < 0)
-    {
-        //perror("Error sending data");
         return 1;
-    }
 
     return 0;
 }
@@ -78,11 +96,8 @@ int read_register(unsigned char address, unsigned char reg, unsigned char *data)
     packets.msgs      = messages;
     packets.nmsgs     = 2;
     if (ioctl(fd, I2C_RDWR, &packets) < 0)
-    {
-        //printf("Reg = %x ", reg);
-        //perror("Error sending data");
         return 1;
-    }
+
     *data = input_buffer;
 
     return 0;
